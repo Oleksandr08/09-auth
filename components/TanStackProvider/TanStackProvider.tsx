1e1713519b483d7f8300cafdc03d9a16ca636ade
Oleksandr08 <is4uk2011@icloud.com>
2025-10-25 23:30:13 +0300

    Update TanStackProvider.tsx

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
-interface Props {
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+type Props = {
   children: React.ReactNode;
-}
-const TanStackProvider = ({ children }: Props) => {
-  const [queryClient] = useState(new QueryClient());
+};
+
+export default function TanStackProvider({ children }: Props) {
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
-};
-
-export default TanStackProvider;
+}
