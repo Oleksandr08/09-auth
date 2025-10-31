51a1adf1c642cacb45817e290c498f3113702bd5
Oleksandr08 <is4uk2011@icloud.com>
2025-10-31 11:37:32 +0200

    Update page.tsx

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,16 +1,12 @@
-import { Metadata } from 'next';
-import { fetchServerNotes } from '@/lib/api/serverApi';
-import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from '@tanstack/react-query';
-import NotesClient from './Notes.client';
-import css from './page.module.css';
+import { Metadata } from "next";
+import { fetchServerNotes } from "@/lib/api/serverApi";
+import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+import css from "./page.module.css";
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { slug } = await params;
-  const category = slug[0] === 'All' ? 'All' : `${slug[0]}`;
+  const category = slug[0] === "All" ? "All" : `${slug[0]}`;
 
   return {
     title: `${category} notes list`,
@@ -18,13 +14,13 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     openGraph: {
       title: `${category} notes list`,
       description: `Easy viewing of ${category} notes list, adding or deleting note`,
-      url: `https://08-zustand-brown-iota.vercel.app/notes/filter/${category}`,
+      url: `https://08-zustand-flame-five.vercel.app/notes/filter/${category}`,
       images: [
         {
-          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
           width: 1200,
           height: 630,
-          alt: 'Notehub App',
+          alt: "Notehub App",
         },
       ],
     },
@@ -37,12 +33,12 @@ type Props = {
 
 export default async function NotesFilterPage({ params }: Props) {
   const { slug } = await params;
-  const category = slug[0] === 'All' ? undefined : slug[0];
+  const category = slug[0] === "All" ? undefined : slug[0];
 
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
-    queryKey: ['notes', { query: '', page: 1, tag: category }],
-    queryFn: () => fetchServerNotes('', 1, category),
+    queryKey: ["notes", { query: "", page: 1, tag: category }],
+    queryFn: () => fetchServerNotes("", 1, category),
   });
 
   return (
